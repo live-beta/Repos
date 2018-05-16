1947f19625f68ca76bdeaa6a09091da4958d956c
live-beta <wanjala.sam.s@gmail.com>
2018-05-16 03:03:16 +0300

    (add menu): add a search menu
    - Creates new menu resource
    - Implements menu item in menu resourse
    - Sets show action if room
    - Inflates menu in oncreateOptions menu
    - Displays toasts
    
    [Builds #Task-03-add-menu]

--- app/src/main/java/com/books/sam/repos/MainActivity.java
+++ app/src/main/java/com/books/sam/repos/MainActivity.java
@@ -1,10 +1,13 @@
 package com.books.sam.repos;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +27,25 @@ public class MainActivity extends AppCompatActivity {
         mSearchResultsTextView = findViewById(R.id.tv_show_repos);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        //action code goes here
+        int itemSelected = item.getItemId();
+        if (itemSelected == R.id.action_search){
+            Context context = MainActivity.this;
+            Toast.makeText(context,"Search Selected",Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
 
 
 }
